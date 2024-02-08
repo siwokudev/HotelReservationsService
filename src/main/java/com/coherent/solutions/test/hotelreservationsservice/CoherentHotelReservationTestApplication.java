fa0d97342d36fef4ee2b9be9740e5ed6d2fd069b
siwoku <ajimenez101287@gmail.com>
2024-02-08 11:48:46 -0600

    project setup

---
+++ src/main/java/com/coherent/solutions/test/hotelreservationsservice/CoherentHotelReservationTestApplication.java
@@ -0,0 +1,13 @@
+package com.coherent.solutions.test.hotelreservationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoherentHotelReservationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoherentHotelReservationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coherent/solutions/test/hotelreservationsservice/CoherentHotelReservationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherent.solutions.test.hotelreservationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoherentHotelReservationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
